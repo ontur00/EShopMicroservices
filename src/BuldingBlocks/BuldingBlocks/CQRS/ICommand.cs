953c60f2a28db3c6e40b6dfce9546bcb5939be27
Artur Garbacik <ontur00@gmail.com>
2025-10-26 13:16:04 +0100

    Before add docker compose

---
+++ src/BuldingBlocks/BuldingBlocks/CQRS/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace BuldingBlocks.CQRS
+{
+    public interface ICommand: ICommand<Unit>   
+    {
+        
+    }
+
+    public interface ICommand<out TResponse> : IRequest<TResponse>
+    {
+    }
+}
